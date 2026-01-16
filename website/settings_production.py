27740e3225ae2eb3bc9b5f6987cf33d3068f78c3
Christoph Reimers <christoph@superservice-international.com>
2026-01-16 11:55:00 +0100

    getting there finally?

--- website/settings_production.py
+++ website/settings_production.py
@@ -5,7 +5,7 @@ DEBUG = False
 ALLOWED_HOSTS = ["brueggerei.superservice-international.com"]
 
 # WhiteNoise for static files
-MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
+# MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
 STORAGES = {
     "default": {
         "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
@@ -20,3 +20,8 @@ AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_ENDPOINT_URL = os.environ.get("AWS_S3_ENDPOINT_URL", "")
+
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://brueggerei.superservice-international.com",
+]
